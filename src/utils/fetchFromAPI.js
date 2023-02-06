431e8ebdb9db41665b44e749110023705d455d69
Vamsi <vamsisai.karumuri@gmail.com>
2023-02-06 23:27:44 +0530

    deploy

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '24793b4fdbmsh9093e016ade6b76p1abe6djsn9d08892091d9',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
